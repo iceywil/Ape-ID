60e76cc00f6bfc69b218ed629999040097684f25
Iceywil <scherrer.wilfried@gmail.com>
2024-11-17 09:09:00 +0700

    a

--- components/register.tsx
+++ components/register.tsx
@@ -8,41 +8,48 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 
-export default function SetText() {
-  const { writeContract } = useWriteContract();
-  const { primaryWallet } = useDynamicContext();
+export default function Register() {
+	const { writeContract } = useWriteContract();
+	const { primaryWallet } = useDynamicContext();
 
-  const [firstName, setFirstName] = useState("Test");
+	const [firstName, setFirstName] = useState("Test");
+	const [data, setData] = useState("");
+	console.log(`firstName : ${firstName}`);
+	console.log(`primaryWallet : ${primaryWallet?.address}`);
 
-  console.log(`firstName : ${firstName}`);
-  console.log(`primaryWallet : ${primaryWallet?.address}`);
+	const wallet = primaryWallet?.address;
 
-  const wallet = primaryWallet?.address;
-  return (
-    <div className="flex flex-row gap-4 justify-center">
-      <img src="/images/ens-mark-Dark_Blue.png" alt="domain" className="h-8" />
-      <Input
-        className="min-w-20"
-        name="myInput"
-        placeholder="Mint a Domain Name"
-        onChange={(e) => setFirstName(e.target.value)}
-      />
-      <Button
-        onClick={() =>
-          writeContract({
-            abi: abi,
-            address: "0x73bb68186837D843804C77871B35A7140d32De39",
-            functionName: "register",
-            args: [
-              firstName,
-              (wallet ||
-                "0x0000000000000000000000000000000000000000") as `0x${string}`,
-            ],
-          })
-        }
-      >
-        Mint
-      </Button>
-    </div>
-  );
+	function register() {
+		let data = writeContract({
+			abi: abi,
+			address: "0x73bb68186837D843804C77871B35A7140d32De39",
+			functionName: "register",
+			args: [
+				firstName,
+				(wallet ||
+					"0x0000000000000000000000000000000000000000") as `0x${string}`,
+			],
+		})
+		if (data != null)
+			setData(data);
+		console.log(`data : ${data}`);
+		}
+
+	return (
+		<div className="flex flex-row gap-4 justify-center">
+			<img src="/images/ens-mark-Dark_Blue.png" alt="domain" className="h-8" />
+			<Input
+				className="min-w-20"
+				name="myInput"
+				placeholder="Mint a Domain Name"
+				onChange={(e) => setFirstName(e.target.value)}
+			/>
+			<Button
+				onClick={register}
+			>
+				Mint
+			</Button>
+			<p>{data}</p>
+		</div>
+	);
 }
