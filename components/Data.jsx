85c96076491149744d71780ab77cc70e2c3405b5
Iceywil <scherrer.wilfried@gmail.com>
2024-11-17 07:17:07 +0700

    a

--- components/Data.jsx
+++ components/Data.jsx
@@ -1,26 +1,23 @@
 "use client";
 
-
-import { useDynamicContext, useIsLoggedIn } from "@dynamic-labs/sdk-react-core";
-import PassportClosed from "./PassportClosed";
-import Register from "./register";
-import SetText from "./setText";
-import { Separator } from "@/components/ui/separator";
-import { IRL } from "./irl";
-import getUserData from "@/app/data/retrieve_user.mjs";
-
-export function Data() {
-	const { user } = useDynamicContext();
-	const isLoggedIn = useIsLoggedIn();
-
-	//const userData = getUserData(user.userId);
-	const verifiedCredentialsArray = user?.verifiedCredentials;
-	console.log("baba", verifiedCredentialsArray);
+function DataComponent({ verifiedCredentialsArray }) {
 	return (
-		<div className="flex flex-row gap-4 justify-center">
-
-			<p>YAYA</p>
-			
-		</div>
+		<ul>
+			{verifiedCredentialsArray.map((credential, index) => (
+				<li key={index}>
+					{credential.oauthProvider && credential.oauthUsername ? (
+						<>
+							{credential.oauthProvider} - {credential.oauthUsername}
+						</>
+					) : credential.address ? (
+						credential.address
+					) : (
+						"No valid data"
+					)}
+				</li>
+			))}
+		</ul>
 	);
 }
+
+export default DataComponent;
